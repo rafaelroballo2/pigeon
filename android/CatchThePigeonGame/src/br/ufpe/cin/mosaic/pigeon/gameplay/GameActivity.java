fb579ada2c8962c757391e0bab866c0b229c4623
Raoni Kulesza <raonikulesza@gmail.com>
2011-04-26 21:15:49 -0300

    Commit inicial do jogo para Android

---
+++ android/CatchThePigeonGame/src/br/ufpe/cin/mosaic/pigeon/gameplay/GameActivity.java
@@ -0,0 +1,28 @@
+package br.ufpe.cin.mosaic.pigeon.gameplay;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class GameActivity extends Activity {
+
+    private GameView mGameView;
+    private TextView mInfoView;
+    private Button mButtonNext;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+ 
+    }
+
+}
---
+++ android/CatchThePigeonGame/src/br/ufpe/cin/mosaic/pigeon/gameplay/GameView.java
@@ -0,0 +1,20 @@
+package br.ufpe.cin.mosaic.pigeon.gameplay;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+//-----------------------------------------------
+
+public class GameView extends View {
+
+    public GameView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        requestFocus();
+
+    }
+
+
+}
+
+
