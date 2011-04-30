ccf3b70678e335db87c63e5cfe36a98654e3e804
marcomachado <masm@cin.ufpe.br>
2011-04-30 16:28:17 -0300

    ajustes no xml

--- android/CatchThePigeon/src/br/ufpe/cin/mosaic/pigeon/client/android/menu/MainActivity.java
+++ android/CatchThePigeon/src/br/ufpe/cin/mosaic/pigeon/client/android/menu/MainActivity.java
@@ -5,34 +5,32 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
-
 import br.ufpe.cin.mosaic.pigeon.client.android.R;
-import br.ufpe.cin.mosaic.pigeon.gameplay.GameActivity;
+import br.ufpe.cin.mosaic.pigeon.client.android.gameplay.GameActivity;
 
 public class MainActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
 
-        findViewById(R.id.start_player).setOnClickListener(
-                new OnClickListener() {
-            public void onClick(View v) {
-                startGame(true);
-            }
-        });
+		findViewById(R.id.start_player).setOnClickListener(
+				new OnClickListener() {
+					public void onClick(View v) {
+						startGame(true);
+					}
+				});
 
-        findViewById(R.id.start_comp).setOnClickListener(
-                new OnClickListener() {
-            public void onClick(View v) {
-                startGame(false);
-            }
-        });
-    }
+		findViewById(R.id.start_comp).setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				startGame(false);
+			}
+		});
+	}
 
-    private void startGame(boolean startWithHuman) {
-        Intent i = new Intent(this, GameActivity.class);
-        startActivity(i);
-    }
+	private void startGame(boolean startWithHuman) {
+		Intent i = new Intent(this, GameActivity.class);
+		startActivity(i);
+	}
 }
