37b4520f9b955b888febcf6131ef7b4746cb3c6a
rafarocha <faelcavalcanti@gmail.com>
2011-05-02 00:14:30 -0300

    initial commit

---
+++ server/src/main/java/br/eng/mosaic/pigeon/common/dto/UserInfo.java
@@ -0,0 +1,13 @@
+package br.eng.mosaic.pigeon.common.dto;
+
+public class UserInfo {
+	
+	public String name;
+	public String email;
+	
+	public UserInfo(String name, String email) {
+		this.name = name;
+		this.email = email;
+	}
+
+}
